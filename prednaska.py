81d9cf7fd0dae35c90b20896e7ed6f8029ed9d21
Netkaa00 <netkaa00@jcu.cz>
2025-10-09 12:11:21 +0000

    lekce 2

---
+++ prednaska.py
@@ -0,0 +1,32 @@
+def vrat_treti(seznam):
+    if len(seznam) >= 3:
+        return seznam[2]
+    else:
+        return None
+    
+def prumer(cislo):
+    return 
+
+    
+
+if __name__ == "__main__":
+    seznam = [12, 50, 1, 3, 5]
+
+    seznam [3] *= 3
+    
+    seznam.append(100)
+
+    seznam.sort()
+    seznam.reverse()
+
+    
+    student = {
+        "Jmeno": "Jan",
+        "prijmeni": "Novak",
+        "vek": 22,
+        "znamky": [1, 2, 2, 3]
+    }
+    student["vek"] += 1
+    print(student["znamky"][3])
+
+
