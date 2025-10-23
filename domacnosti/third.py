fd2f1448d9e1a89a978d04178c6d3104281ce4ee
Netkaa00 <netkaa00@jcu.cz>
2025-10-23 12:48:02 +0000

    cv4

---
+++ domacnosti/third.py
@@ -0,0 +1,28 @@
+
+def je_prvocislo(cislo):
+
+    if cislo <= 1:
+        return False
+
+
+    for i in range(2, int(cislo**0.5) + 1):
+        if cislo % i == 0:
+            return False
+
+    return True
+
+
+def vrat_prvocisla(maximum):
+    
+    prvocisla = []
+
+    for i in range(2, maximum + 1):
+        if je_prvocislo(i):
+            prvocisla.append(i)
+            
+    return prvocisla
+
+if __name__ == "__main__":
+    cislo = int(input("Zadej maximum: "))
+    prvocisla = vrat_prvocisla(cislo)
+    print(prvocisla) 
