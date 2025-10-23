14c6f020b7dd941cf1120efed18d773583551699
Netkaa00 <netkaa00@jcu.cz>
2025-10-23 12:47:46 +0000

    cviceni 4

---
+++ cviceni4_1.py
@@ -0,0 +1,52 @@
+def levensteinova_vzdalenost(dotaz1, dotaz2):
+    """
+    Levensteinova vzdalenost říka, jak jsou 2 řetězce rozdílné, pokud jsou stejné je Levensteinova vzdalenost 0,
+    pro řetězce "čas" a "čaj" je Levensteinova vzdalenost 1 (liší se v 1 písmenu)
+    """
+
+    lenght = max(len(dotaz1), len(dotaz2))
+    i = 0
+
+    while i < lenght:
+        if i < len(dotaz1) or i < dotaz2:
+            if dotaz1 [i] != dotaz2[i]:
+                levenstain += 1
+        else:
+            levenstain += 1
+        i += 1
+    return levenstain
+
+    
+
+
+def levensteinova_vzdalenost_for(dotaz1, dotaz2):
+    lenght = min(len(dotaz1), len(dotaz2))
+    levenstein = 0
+    for i in range(lenght):
+        if dotaz1[i] != dotaz2[i]:
+            levenstein += 1
+
+    levenstein += abs(len(dotaz1) - len(dotaz2))
+    return levenstein
+        
+   
+
+
+ 
+ 
+
+
+
+
+if __name__ == "__main__":
+
+    query1 = "seznam"
+    query2 = "seznamka"
+    query3 = "sesnam"
+    query4 = "seznam"
+
+    print(levensteinova_vzdalenost(query1, query2))
+    print(levensteinova_vzdalenost(query2, query3))
+    print(levensteinova_vzdalenost(query1, query3))
+
+    print(levensteinova_vzdalenost(query1, query4))
