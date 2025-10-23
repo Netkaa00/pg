ddd115884c99c303461f8775f6bf626c236e7ab0
Netkaa00 <netkaa00@jcu.cz>
2025-10-23 07:08:46 +0000

    lekce 4

---
+++ prednaska 4.py
@@ -0,0 +1,55 @@
+
+
+def vrat_3_hodnoty():
+    return 1 , 2 , 3
+
+if __name__ == "__main__":
+
+    student = {
+        "jmeno": "Alice",
+        "Age": 25,
+
+    }
+
+    print("jmeno" in student.keys())
+
+    
+
+    #prvni, druhy, treti =  vrat_3_hodnoty()
+    #print(prvni)
+    #print(druhy)
+    #print(treti)
+
+
+
+
+    #fronta = [] 
+    #fronta.append(10)
+    #fronta.append(5)
+    #fronta.append(1)
+    #fronta.append(20)
+
+    #print(fronta.pop(0))  
+
+    #fronta.append(3)
+
+    #print(fronta.pop(0))
+    #print(fronta.pop(0))    
+    #print(fronta.pop(0))
+    #print(fronta.pop(0))
+
+    #print("---")
+
+    #zasobnik = []
+    #zasobnik.append(10)
+    #zasobnik.append(5)
+    #zasobnik.append(1)
+    #zasobnik.append(20)
+    
+    #print(zasobnik.pop(0))
+
+    #print(zasobnik.pop(0))
+    #print(zasobnik.pop(0))
+    #print(zasobnik.pop(0))
+    #print(zasobnik.pop(0))
+    
